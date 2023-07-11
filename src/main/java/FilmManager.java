5beded45f530749682e4484aaf17a0c0ac911c60
Yrova <yarovavaleriya@gmail.com>
2023-07-11 08:11:31 +0300

    Reformat code

--- src/main/java/FilmManager.java
+++ src/main/java/FilmManager.java
@@ -5,33 +5,36 @@ public class FilmManager {
     public FilmManager() {
         this.limit = 5;
     }
+
     public FilmManager(int limit) {
         this.limit = limit;
     }
 
     public void newfilm(String film) {
         String[] tmp = new String[films.length + 1];
-        for (int i =0; i < films.length; i++) {
+        for (int i = 0; i < films.length; i++) {
             tmp[i] = films[i];
         }
         tmp[tmp.length - 1] = film;
         films = tmp;
     }
+
     public String[] findAll() {
         return films;
     }
+
     public String[] findLast() {
         int resultLength;
         if (films.length < limit) {
             resultLength = films.length;
         } else {
-        resultLength = limit;
+            resultLength = limit;
         }
 
         String[] tmp = new String[resultLength];
-        for (int i =0; i < tmp.length; i++) {
+        for (int i = 0; i < tmp.length; i++) {
             tmp[i] = films[films.length - 1 - i];
-    }
+        }
         return tmp;
     }
 }
